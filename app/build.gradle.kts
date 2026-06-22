bc46b68e0a4dd7f5c540d0c3ce04c30d3fb42465
POL0i <dennispolonioapazachavez@gmail.com>
2026-06-22 11:42:24 -0400

    Bump version to 1.0.2 for F-Droid reproducible builds

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.polonio.duwave"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.2"
     }
 
     val keystoreFile = file("../release.keystore")
