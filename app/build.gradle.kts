285e08270822a6319d61948242319aa0d1ea7c5f
POL0i <dennispolonioapazachavez@gmail.com>
2026-06-21 20:22:01 -0400

    Downgrade compileSdk to 35 for F-Droid compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,11 +7,11 @@ plugins {
 
 android {
     namespace = "com.example.beatpulse"
-    compileSdk = 36
+    compileSdk = 35
     defaultConfig {
         applicationId = "com.polonio.duwave"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
     }
