8b3bb0e3f3850fde50f0a10da252ace6eee6b0c7
POL0i <dennispolonioapazachavez@gmail.com>
2026-09-22 11:00:20 -0400

    Bump version to 2.1.1 (versionCode 11)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -131,8 +131,8 @@ android {
         applicationId = "com.polonio.duwave"
         minSdk = 24
         targetSdk = 35
-        versionCode = 10
-        versionName = "2.1.0"
+        versionCode = 11
+        versionName = "2.1.1"
         resConfigs("en", "es")
     }
     
